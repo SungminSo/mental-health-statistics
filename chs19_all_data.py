4633615a457dc64a3a2ca78547d8afd0ff2d628a
SungminSo <soos196@hanyang.ac.kr>
2020-10-02 18:31:17 +0900

    구 전체면적 대비 공원면적 기준으로 상관관계 계산

--- chs19_all_data.py
+++ chs19_all_data.py
@@ -1,6 +1,33 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 data = pd.read_csv('chs19_seoul_sample.csv', 'r', encoding='latin', delimiter=',', low_memory=False)
-print(data.describe())
+# print(data.describe())
 
-print(data.groupby('signgu_code').describe())
+# 거동이 불편한 사람 제외
+is_qoc_01z1 = data['qoc_01z1'] == 1
+
+# 흡연자 제외
+is_sma_01z1 = data['sma_01z1'] == 3
+
+tmp_data = data[is_qoc_01z1 & is_sma_01z1]
+
+park_rate_data = pd.read_csv('구별 공원면적 비율.txt', 'r', encoding='utf-8', delimiter='\t')
+
+merged_data = pd.merge(tmp_data, park_rate_data, how='inner', left_on='signgu_code', right_on='구분')
+# print(merged_data.loc[:, ['signgu_code', 'mta_01z1', 'mta_02z1', 'mtb_01z1', '구분', '공원율(%)']].head())
+
+print(merged_data.head())
+# print(merged_data.mean())
+
+print(merged_data.groupby('signgu_code').mean())
+
+# 구 전체면적 대비 공원면적
+print('주관적 스트레스 기준:', merged_data['mta_01z1'].corr(merged_data['공원율(%)']))
+print('스트레스 인한 정신상담 여부:', merged_data['mta_02z1'].corr(merged_data['공원율(%)']))
+print('우울감 경험 여부:', merged_data['mtb_01z1'].corr(merged_data['공원율(%)']))
+print('행복감 지수:', merged_data['qoc_07z1'].corr(merged_data['공원율(%)']))
+#
+# print(merged_data[''])
+
--- file_convertor.py
+++ file_convertor.py
@@ -1,26 +1,3 @@
-import pandas as pd
-
-# file = pd.read_csv('chs19_all.txt', 'r', encoding='latin', delimiter='\t', low_memory=False)
-# print(file.head(10))
-# file.to_csv('chs19_all.csv', index=False)
-
-# data = pd.read_csv('chs19_all.csv', 'r', encoding='latin', delimiter=',', low_memory=False)
-# print(data.head())
-# print(data.columns)
-# print(data.shape)
-
-data = pd.read_csv('chs19_all_sample.csv', 'r', encoding='latin', delimiter=',', low_memory=False)
-print(data.head())
-
-is_seoul = data['signgu_code'] <= 20000
-seoul_sample = data[is_seoul]
-seoul_sample.to_csv('chs19_seoul_sample.csv', index=False)
-
-# print(data.iloc[:, [3, 4]].head())
-
-# data_sample = data.loc[:, ['age', 'sex', 'signgu_code', 'fma_19z1', 'mea_14z1', 'mea_10z1', 'sma_01z1', 'sma_03z2', 'dra_01z1', 'mta_01z1', 'mta_02z1', 'mtb_01z1', 'qoc_07z1', 'qoc_01z1', 'hma_01z2', 'enb_02z1', 'enb_03z1', 'soa_01z1']]
-# print(data_sample.head(10))
-# data_sample.to_csv('chs19_all_sample.csv', index=False)
 
 # with open('chs19_all.txt', 'r', encoding='CP949') as f:
 #     line_num = 1
@@ -31,3 +8,29 @@ seoul_sample.to_csv('chs19_seoul_sample.csv', index=False)
 #         line_num += 1
 #     print(line_num)
 #     f.close()
+
+import pandas as pd
+
+# file = pd.read_csv('chs19_all.txt', 'r', encoding='latin', delimiter='\t', low_memory=False)
+# print(file.head(10))
+# file.to_csv('chs19_all.csv', index=False)
+
+# data = pd.read_csv('chs19_all.csv', 'r', encoding='latin', delimiter=',', low_memory=False)
+# print(data.head())
+# print(data.columns)
+# print(data.shape)
+# data_sample = data.loc[:, ['age', 'sex', 'signgu_code', 'fma_19z1', 'mea_14z1', 'mea_10z1', 'sma_01z1', 'sma_03z2', 'dra_01z1', 'mta_01z1', 'mta_02z1', 'mtb_01z1', 'qoc_07z1', 'qoc_01z1', 'hma_01z2', 'enb_02z1', 'enb_03z1', 'soa_01z1']]
+# print(data_sample.head(10))
+# data_sample.to_csv('chs19_all_sample.csv', index=False)
+
+# data = pd.read_csv('chs19_all_sample.csv', 'r', encoding='latin', delimiter=',', low_memory=False)
+# print(data.head())
+#
+# is_seoul = data['signgu_code'] <= 20000
+# seoul_sample = data[is_seoul]
+# seoul_sample.to_csv('chs19_seoul_sample.csv', index=False)
+
+file = pd.read_csv('구별 공원면적 비율.txt', 'r', encoding='utf-8', delimiter='\t')
+print(file.head())
+
+
