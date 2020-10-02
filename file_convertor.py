0aebc730eee846781cc7fcf110bd78c934d349f0
SungminSo <soos196@hanyang.ac.kr>
2020-10-02 15:17:48 +0900

    chs19_all.csv 파일 만들었지만 용량이 너무 크다

--- file_convertor.py
+++ file_convertor.py
@@ -1,14 +1,15 @@
-# import pandas as pd
-#
-# file = pd.read_fwf('chs19_all.txt')
-# file.to_csv('chs19_all.csv')
+import pandas as pd
 
-with open('chs19_all.txt', 'r', encoding='UTF8') as f:
-    line_num = 1
-    line = f.readline()
-    while line:
-        print('%d %s' % (line_num, line))
-        line = f.readline()
-        line_num += 1
-    print(line_num)
-    f.close()
+file = pd.read_csv('chs19_all.txt', 'r', encoding='latin', delimiter='\t', low_memory=False)
+print(file.head(10))
+file.to_csv('chs19_all.csv', index=False)
+
+# with open('chs19_all.txt', 'r', encoding='CP949') as f:
+#     line_num = 1
+#     line = f.readline()
+#     while line:
+#         print('%d %s' % (line_num, line))
+#         line = f.readline()
+#         line_num += 1
+#     print(line_num)
+#     f.close()
